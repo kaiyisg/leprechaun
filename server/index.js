39f4117e9ebcdb36e68cbe590ed3634659d7454b
Jeremy Hon <jeremy.hon.gy@gmail.com>
2018-06-24 00:44:25 +0800

    add test endpoint

--- server/index.js
+++ server/index.js
@@ -67,6 +67,10 @@ app.get('/api/payroll', async (req, res, next) => {
   return res.send(data)
 })
 
+app.get('/api/verion', async (req, res) => {
+  return res.send('v1.0')
+})
+
 const clockIn = () => {
   io.emit('clockin', {
     phoneNumber: NUMBER,
