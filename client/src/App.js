88bce1804d1d0348ce408789a276507facd4f9af
Kai Yi Lee <kaiyisg@yahoo.com.sg>
2018-06-23 19:21:49 +0800

    Adding the antd

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
 
-import logo from "./logo.svg";
 import "./App.css";
 import { subscribeToTimer } from "./api/socket";
+import { Layout, Menu, Breadcrumb } from "antd";
+
+const { Header, Content, Footer } = Layout;
 
 class App extends Component {
   state = {
@@ -32,15 +34,38 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">{this.state.response}</p>
-        <p className="App-socket">
-          This is the timer value: {this.state.timestamp}
-        </p>
+      <div className="App" style={{ height: "100%" }}>
+        <Layout className="layout" style={{ height: "100%" }}>
+          <Header>
+            <div className="logo" />
+            <Menu
+              theme="dark"
+              mode="horizontal"
+              defaultSelectedKeys={["2"]}
+              style={{ lineHeight: "64px" }}
+            >
+              <Menu.Item key="1">nav 1</Menu.Item>
+              <Menu.Item key="2">nav 2</Menu.Item>
+              <Menu.Item key="3">nav 3</Menu.Item>
+            </Menu>
+          </Header>
+          <Content style={{ padding: "0 50px" }}>
+            <Breadcrumb style={{ margin: "16px 0" }}>
+              <Breadcrumb.Item>Home</Breadcrumb.Item>
+              <Breadcrumb.Item>List</Breadcrumb.Item>
+              <Breadcrumb.Item>App</Breadcrumb.Item>
+            </Breadcrumb>
+            <div style={{ background: "#fff", padding: 24, minHeight: 280 }}>
+              <p className="App-intro">{this.state.response}</p>
+              <p className="App-socket">
+                This is the timer value: {this.state.timestamp}
+              </p>
+            </div>
+          </Content>
+          <Footer style={{ textAlign: "center" }}>
+            Ant Design ©2016 Created by Ant UED
+          </Footer>
+        </Layout>
       </div>
     );
   }
