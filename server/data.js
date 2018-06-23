30e0e04d53d4c2d33ba7e4176aba861ede0025f4
Kai Yi Lee <kaiyisg@yahoo.com.sg>
2018-06-23 21:55:51 +0800

    Reformat data

--- server/data.js
+++ server/data.js
@@ -1,8 +1,56 @@
 const { NUMBER } = require('./config')
+const moment = require('moment')
 
 const data = [
   {
     key: '1',
+    name: 'Janson Seah',
+    startTime: moment()
+      .add(-1, 'd')
+      .format('ddd, MMM DD, h:mm:ss a'),
+    endTime: moment()
+      .add(-1, 'd')
+      .add(3, 'h')
+      .format('ddd, MMM DD, h:mm:ss a'),
+    rate: '$13.50',
+    cost: '$40.50',
+    paid: 'Yes',
+    phoneNumber: '',
+  },
+  {
+    key: '2',
+    name: 'Huang Yixuan',
+    startTime: moment()
+      .add(-1, 'd')
+      .add(1, 'h')
+      .format('ddd, MMM DD, h:mm:ss a'),
+    endTime: moment()
+      .add(-1, 'd')
+      .add(4, 'h')
+      .format('ddd, MMM DD, h:mm:ss a'),
+    rate: '$13.50',
+    cost: '$40.50',
+    paid: 'Yes',
+    phoneNumber: '',
+  },
+  {
+    key: '3',
+    name: 'Lee Kai Yi',
+    startTime: moment()
+      .add(-1, 'd')
+      .add(1, 'h')
+      .format('ddd, MMM DD, h:mm:ss a'),
+    endTime: moment()
+      .add(-1, 'd')
+      .add(7, 'h')
+      .format('ddd, MMM DD, h:mm:ss a'),
+    rate: '$13.50',
+    cost: '$81.00',
+    paid: 'Yes',
+    phoneNumber: '',
+  },
+  {
+    key: '4',
     name: 'Jeremy Hon',
     startTime: '',
     endTime: '',
@@ -10,36 +58,6 @@ const data = [
     cost: '',
     paid: '',
     phoneNumber: NUMBER,
-  },
-  {
-    key: '2',
-    name: 'Janson Seah',
-    startTime: '',
-    endTime: '',
-    rate: '$13.50',
-    cost: '',
-    paid: '',
-    phoneNumber: '',
-  },
-  {
-    key: '3',
-    name: 'Huang Yixuan',
-    startTime: '',
-    endTime: '',
-    rate: '$13.50',
-    cost: '',
-    paid: '',
-    phoneNumber: '',
-  },
-  {
-    key: '4',
-    name: 'Lee Kai Yi',
-    startTime: '',
-    endTime: '',
-    rate: '$13.50',
-    cost: '',
-    paid: '',
-    phoneNumber: '',
   },
 ]
 
