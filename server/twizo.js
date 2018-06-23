fe51975eea80f73aab94b073337c7306005b5193
Jeremy Hon <jeremy.hon.gy@gmail.com>
2018-06-23 18:05:22 +0800

    add twizo skeleton

---
+++ server/twizo.js
@@ -0,0 +1,8 @@
+const axios = require("axios");
+
+const TEST_KEY = "5RX7BZX9R287D3d-hKCdg7t4rYxZJLqk7YvRTX4Skrb1zKQ_";
+const REAL_KEY = "t2egHLRt2GjYT35T5mKaTAIYy7GzVvGYKjw5Mob31OSa6k7y";
+
+// const testTwizo = axio
+
+const registerBioVoice = number => {};
